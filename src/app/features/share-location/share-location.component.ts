6aedf0bf0b44e80332cc5daf015438fb775504d9
rasekh moafi <rasekhmoafi@gmail.com>
2023-01-23 15:20:01 +0330

    added marker

--- src/app/features/share-location/share-location.component.ts
+++ src/app/features/share-location/share-location.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-share-location',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./share-location.component.scss']
 })
 export class ShareLocationComponent implements OnInit {
+  private map: any;
+  private marker: any;
+  longitude: number | undefined;
+  latitude: number | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initMap()
+  }
+
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [  23.35353, 53.2565 ],
+      zoom: 6,
+      zoomControl: false
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 4,
+      attribution: ''
+    });
+    tiles.addTo(this.map);
+    this.map.on('click', (e :any) => {
+      if(this.marker) {
+        this.map.removeLayer(this.marker)
+      }
+      console.log(e.latlng);
+      this.latitude = e.latlng.lat;
+        this.longitude = e.latlng.lng;
+        this.marker = L.marker([this.latitude!, this.longitude!]).addTo(this.map);
+    });
   }
 
 }
